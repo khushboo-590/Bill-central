29179f90ef04759ceed36dd5293160a83f86c8c7
khushboo-590 <khushbook4979@gmail.com>
2025-05-07 10:09:23 +0530

    margin remove

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,30 +1,34 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import {slides} from '../utils/helper' 
+import { Autoplay } from 'swiper/modules';
+
+import { slides } from '../utils/helper'
 
 
 const Slider = () => {
     return (
-         <section className="bg-white">
+        <section className="bg-white">
             <div className="container max-w-[1140px] mx-auto p-4">
-                <h2 className="font-normal text-[30px] md:text-[38px] leading-[110%] text-[#003459] max-w-[548px] mt-[45px] text-center md:text-left ">
+                <h2 className="font-normal text-[30px] md:text-[38px] leading-[110%] text-[#003459] lg:max-w-[548px] mt-[45px] text-center lg:text-left ">
                     More than
                     <span className="text-[#00A8E8]"> 80,000+</span> companies trust bill central
                 </h2>
 
+
                 <Swiper
-                    className=" mt-[20px] md:mt-[65px] mb-[30px] lg:mb-[162px]"
-                    spaceBetween={50}
-                    slidesPerView={6}
+                    modules={[Autoplay]}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    }}
+                    className="mySwiper mt-[30px] md:mt-[65px] mb-[30px] lg:mb-[162px] mx-auto"
                     breakpoints={{
                         1024: { slidesPerView: 6, spaceBetween: 30 },
                         768: { slidesPerView: 4, spaceBetween: 20 },
                         640: { slidesPerView: 3, spaceBetween: 10 },
                         320: { slidesPerView: 2, spaceBetween: 10 },
                     }}
-                    onSlideChange={() => console.log('slide change')}
-                    onSwiper={(swiper) => console.log(swiper)}
                 >
                     {slides.map((src, idx) => (
                         <SwiperSlide key={idx}>
