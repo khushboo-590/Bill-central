d3afe29b4c2ad1a53640a9db43fa07ca2a4c7394
khushboo-590 <khushbook4979@gmail.com>
2025-05-10 09:02:27 +0530

    eamil update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
                         <h1 className='text-[35px] sm:text-[50px] lg:text-[60px] font-normal text-white leading-[110%] md:max-w-[506px]'>Easily Compare <span className='font-bold'>Energy, Gas, And Internet</span> Plans</h1>
                         <CustomContent content="Find and switch to the best energy, gas, and internet plans with ease. Our platform simplifies your search, letting you compare providers and make informed decisions, all in one place." className="text-white mt-4 opacity-90"/>
                         <div className="flex items-center relative max-w-[476px] mt-[30px] md:mt-[42px]">
-                            <CustomInput type="text" placeholder="Start typing your address" />
+                            <CustomInput type="text" placeholder="Start typing your address" inputClass="pr-[160px]" />
                             <Custombtn btnClass="absolute z-2 right-[10px]" btnText="Compare" whitesvg={<svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1 14.3691L14 1.36914M14 1.36914V13.8491M14 1.36914H1.52" stroke="white" stroke-width="1.93338" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
--- src/components/common/CustomInput.jsx
+++ src/components/common/CustomInput.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const CustomInput = ({ type, placeholder }) => {
+const CustomInput = ({ type, placeholder, inputClass }) => {
     return (
-        <input className='font-normal text-base leading-[110%] text-[#00171F80] z-1 outline-0 border-[#00000033] w-full bg-[#FEFEFE] px-[22px] border max-w-[476px] h-[66px] rounded-tr-[100px] rounded-br-[100px] rounded-bl-[50px]' type={type} placeholder={placeholder} />
-    )
+        <input
+            className={`font-normal text-base leading-[110%] text-[#00171F80] z-1 outline-0 border-[#00000033] w-full bg-[#FEFEFE] px-[22px] border max-w-[476px] h-[66px] rounded-tr-[100px] rounded-br-[100px] rounded-bl-[50px] ${inputClass}`}
+            type={type}
+            placeholder={placeholder}
+        />    )
 }
 
 export default CustomInput
+
